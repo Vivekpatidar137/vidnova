1bdb75052f70e385eab20b24cfacf753db74b297
Vivek Patidar <vivekpatidar775@gmail.com>
2024-12-18 18:12:14 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -78,7 +78,7 @@ const Header = () => {
           {showSearchInput || (
             <form className="hidden md:flex items-center flex-grow">
               <input
-                className="flex-grow border border-gray-300 rounded-l-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="flex-grow border border-gray-300 rounded-l-full px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 type="text"
                 placeholder="Search"
                 onFocus={() => setShowSuggestions(true)}
@@ -120,7 +120,7 @@ const Header = () => {
           ) : (
             <form className="flex items-center flex-grow">
               <input
-                className="flex-grow border border-gray-300 rounded-l-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="flex-grow border border-gray-300 rounded-l-full px-4 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
                 type="text"
                 placeholder="Search"
                 onFocus={() => setShowSuggestions(true)}
