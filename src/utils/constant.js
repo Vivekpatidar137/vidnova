1bca911bec8c8a7ebfc76f71b65fd947e9b09cbc
Vivek Patidar <vivekpatidar775@gmail.com>
2025-01-04 12:07:27 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,9 +5,9 @@ export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const VIDEO_SUGGESTIONS =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=";
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=";
 
 export const SEARCH_LIST =
-  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=";
+  "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=";
 
 export const API_KEY = process.env.REACT_APP_API_KEY;
