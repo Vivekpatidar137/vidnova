5a2bc66b783c95ea0a858e5023b49a63d043148b
Vivek Patidar <vivekpatidar775@gmail.com>
2025-02-19 19:58:53 +0530

    Fix: CORS Error in YouTube Search Suggestions API
    Changes Made
    
    Updated the YOUTUBE_SEARCH_API constant to use a CORS proxy
    Changed from direct API access to using thingproxy.freeboard.io as intermediary
    Added proper protocol (HTTPS) and required /fetch/ path segment

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@ export const YouTUBE_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=";
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://thingproxy.freeboard.io/fetch/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const VIDEO_SUGGESTIONS =
   "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=";
